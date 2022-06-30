fc322970f16585389c0f7e7e48600f595ba3c111
Jefferson Carlos Lima <92160349+JeffersonCarlosLima@users.noreply.github.com>
2022-06-30 11:23:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function() {
     // Necessário declarar a variável img 
-    var img = ["img/1.png", "img/2.png", "img/3.png", "img/4.png", "img/5.png", "img/6.png"];
+    var img = ["img/1.png", "img/2.png", "img/3.png", "img/4.png", "img/5.png", "img/6.png", "img/7.png", "img/8.png"];
     var mudar = Math.floor(Math.random() * img.length);
     let classe = "imagem-aniversario"
     document.getElementById("imagem").innerHTML = "<img src='" + img[mudar] + " '>";
